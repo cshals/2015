728cc2234f9a368497fd1e95d091352f9b4d764d
Jonas Almeida <jalmeida@mathbiol.org>
2014-12-17 14:28:37 -0500

    reactive pages

--- javascripts/cshals2015.js
+++ javascripts/cshals2015.js
@@ -1,1 +1,22 @@
 console.log('cshals2015.js')
+
+cshals2015=function(opt){
+    opt+=".html";
+    cshals2015.load("javascripts/"+opt,function(h){
+        var s = document.getElementById('section')
+        s.innerHTML=h
+    })
+    console.log(opt)
+}
+
+cshals2015.load = function(url,fun){ // XMLHttpRequest
+    var r = new XMLHttpRequest();
+    r.onload=function(x){
+        fun(x.target.responseText)
+    }
+    r.open("GET",url,true);
+    r.send();
+}
+
+// ini
+cshals2015('home')
